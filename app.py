8f74285b0afdb3dbc5343df9cb13ac8cf0641ccd
Vinícius Soares <vinicius4burame@gmail.com>
2025-06-18 13:30:22 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+# streamlit_app.py
+import streamlit as st
+#import kagglehub
+
+# Configuração da página
+#st.set_page_config(
+#    page_title="Game Engagement Analysis",
+#    layout="centered",
+#    page_icon="🎮"
+#)
+
+# Título do aplicativo
+st.title("🎮 Análise de Engajamento em Jogos")
+
+
+# Rodapé
+#st.markdown("---")
+#st.caption("Desenvolvido com Streamlit | Modelo treinado com PyCaret")
